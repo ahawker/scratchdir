a7bae1f93373e91a1d4f36a2798cdc4d35bf23e8
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-09-09 15:34:42 -0700

    Add support for py37 and py38

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 def get_long_description():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 
@@ -27,6 +27,7 @@ setup(
     license='Apache 2.0',
     description='Context manager used to maintain your temporary directories/files.',
     long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     py_modules=['scratchdir'],
     classifiers=(
         'Development Status :: 5 - Production/Stable',
@@ -37,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules'
     )
 )
